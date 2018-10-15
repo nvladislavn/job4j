1704a76df1977b3feda9cc20b5e4b9ec5779d695
nvladislavn <nvladislavn@gmail.com>
2018-10-15 19:20:08 +0500

    4.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -7,7 +7,24 @@ package ru.job4j.max;
  */
 public class Max {
 
+    /**
+     * Maximum of two numbers
+     * @param first
+     * @param second
+     * @return maximum of two numbers
+     */
     public int max(int first, int second) {
             return first > second ? first : second;
+    }
+
+    /**
+     * Maximum of three numbers
+     * @param first
+     * @param second
+     * @param third
+     * @return maximum of three numbers
+     */
+    public int max(int first, int second, int third) {
+        return this.max(max(first, second), third);
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.max;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,8 +14,10 @@ import static org.junit.Assert.assertThat;
  */
 public class MaxTest {
 
+    //Maximum of two numbers
+
     /**
-     * Test the max method, when the first mumber is less than the second
+     * Test the max method, when the first number is less than the second
      */
     @Test
     public void whenFirstLessThanSecond() {
@@ -40,6 +43,48 @@ public class MaxTest {
     public void whenFirstEqualSecond() {
         Max maxim = new Max();
         int result = maxim.max(1, 1);
+        assertThat(result, is(1));
+    }
+
+    //Maximum of three numbers
+
+    /**
+     * Test the max method, when the first number is maximum
+     */
+    @Test
+    public void whenTheMaximumIsTheFirst() {
+        Max max = new Max();
+        int result = max.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    /**
+     * Test the max method, when the second number is maximum
+     */
+    @Test
+    public void whenTheMaximumIsTheSecond() {
+        Max max = new Max();
+        int result = max.max(2, 3, 1);
+        assertThat(result, is(3));
+    }
+
+    /**
+     * Test the max method, when the third number is maximum
+     */
+    @Test
+    public void whenTheMaximumIsTheThird() {
+        Max max = new Max();
+        int result = max.max(2, 1, 3);
+        assertThat(result, is(3));
+    }
+
+    /**
+     * Test the max method, when three numbers are equal
+     */
+    @Test
+    public void whenThreeEqualNumbers() {
+        Max max = new Max();
+        int result = max.max(1, 1, 1);
         assertThat(result, is(1));
     }
 }
