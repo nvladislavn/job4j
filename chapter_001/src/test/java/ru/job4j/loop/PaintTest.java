1c0a1001f0ebd6a959733e6cf7445d7bdc14f0c4
nvladislavn <nvladislavn@gmail.com>
2018-10-17 21:16:56 +0500

    5.4. Построить пирамиду в псевдографике.[#76696] с исправлениями 1

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
-
 import java.util.StringJoiner;
 
 import static org.hamcrest.core.Is.is;
@@ -17,7 +16,39 @@ import static org.junit.Assert.assertThat;
 public class PaintTest {
 
     /**
-     * Test paint method
+     * Test whenPyramid4Right
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()));
+    }
+
+    /**
+     * Test whenPyramid4Left
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()));
+    }
+
+    /**
+     * Test pyramid method
      */
     @Test
     public void whenPyramidOf4Rows() {
@@ -29,5 +60,4 @@ public class PaintTest {
                                                         .add("^^^^^^^")
                                                         .toString()));
     }
-
 }
