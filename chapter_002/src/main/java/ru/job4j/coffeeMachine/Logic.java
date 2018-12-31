3e5a82c3ab92820b424e43bbaf91187aa87f66b3
nvladislavn <nvladislavn@gmail.com>
2018-12-31 12:37:12 +0500

    Кофемашина. [#34741]. Доработка 1.

--- chapter_002/src/main/java/ru/job4j/coffeeMachine/Logic.java
+++ chapter_002/src/main/java/ru/job4j/coffeeMachine/Logic.java
@@ -21,20 +21,21 @@ public class Logic {
     public int[] getChange(int value, int price) {
         int change = value - price;
         Coin[] coinArray = Coin.values();
-        int[] resultArray = new int[0];
+        int[] resultArray = new int[change];
+        int index = 0;
         for (Coin coin : coinArray) {
             int coinValue = coin.getCoinValue();
             int coinCount = 0;
             if (coinValue <= change) {
                 coinCount = change / coinValue;
                 change = change % coinValue;
-                int arrayLength = resultArray.length;
-                resultArray = Arrays.copyOf(resultArray, arrayLength + coinCount);
                 for (int i = 0; i < coinCount; i++) {
-                    resultArray[arrayLength + i] = coinValue;
+                    resultArray[index + i] = coinValue;
                 }
+                index += coinCount;
             }
         }
+        resultArray = Arrays.copyOf(resultArray, index);
         return resultArray;
     }
 }
