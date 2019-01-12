07b7bde4b377e434dd8d20f17cd7b043d60b7530
nvladislavn <nvladislavn@gmail.com>
2019-01-12 20:12:27 +0500

    Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * SortUser
@@ -21,5 +19,47 @@ public class SortUser {
      */
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    /**
+     * sortNameLength.
+     * sorted users list by name length.
+     *
+     * @param list - the users list.
+     * @return - the sorted users list.
+     */
+    public List<User> sortNameLength(List<User> list) {
+        Comparator<User> comp = new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return Integer.compare(user1.getName().length(), user2.getName().length());
+            }
+        };
+        Collections.sort(list, comp);
+        return list;
+    }
+
+    /**
+     * sortByFields
+     * sorts list first by name, then by age.
+     * @param list - users list.
+     * @return - sorted users list.
+     */
+    public List<User> sortByFields(List<User> list) {
+        Comparator<User> nameComparator = new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return user1.getName().compareTo(user2.getName());
+            }
+        };
+        Comparator<User> ageComparator = new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return Integer.compare(user1.getAge(), user2.getAge());
+            }
+        };
+        Comparator<User> comp = nameComparator.thenComparing(ageComparator);
+        Collections.sort(list, comp);
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -15,22 +16,64 @@ import static org.junit.Assert.*;
  */
 public class SortUserTest {
 
+    SortUser sort;
+    StringBuilder actual;
+
+    @Before
+    public void createSortUser() {
+        this.sort = new SortUser();
+        this.actual = new StringBuilder();
+    }
+
     /**
      * tests sort.
      */
     @Test
    public void whenJohnKateMichelleThenKateMichelleJohn() {
-        SortUser sort = new SortUser();
         List<User> userList = new ArrayList<>(Arrays.asList(
                                                         new User("John", 45),
                                                             new User("Kate", 25),
                                                             new User("Michelle", 35)
-                                                            ));
-        String expected = "KateMichelleJohn";
-        StringBuilder actual = new StringBuilder(16);
+                                                            )
+                                                );
         for (User user : sort.sort(userList)) {
             actual.append(user.getName());
         }
-        assertThat(actual.toString(), is(expected));
+        assertThat(actual.toString(), is("KateMichelleJohn"));
+    }
+
+    /**
+     * tests sortNameLength.
+     */
+    @Test
+    public void whenJohnMichelleGarryThenJohnGarryMichelle() {
+        List<User> userList = new ArrayList<>(Arrays.asList(
+                                                        new User("John", 45),
+                                                        new User("Michelle", 25),
+                                                        new User("Garry", 35)
+                                                            )
+                                                );
+        for (User user : this.sort.sortNameLength(userList)) {
+            actual.append(user.getName());
+        }
+        assertThat(actual.toString(), is("JohnGarryMichelle"));
+    }
+
+    /**
+     * tests sortByFields.
+     */
+    @Test
+    public void whenJohnKateJohnThenJohnJohnKate() {
+        List<User> userList = new ArrayList<>(Arrays.asList(
+                                                        new User("John", 45),
+                                                        new User("Kate", 25),
+                                                        new User("John", 35),
+                                                        new User("Kate", 20)
+                                                        )
+                                                );
+        for (User user : this.sort.sortByFields(userList)) {
+            actual.append(user.getName()).append(user.getAge());
+        }
+        assertThat(actual.toString(), is("John35John45Kate20Kate25"));
     }
 }
