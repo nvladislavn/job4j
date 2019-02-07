8be37086eaa94dac6a5d42e5bb05a9264295ae2f
nvladislavn <nvladislavn@gmail.com>
2019-02-07 20:33:52 +0500

    Подсчет функции в диапазоне. [#24251]/ Исправление 1

--- chapter_004/src/main/java/ru/job4j/Calculator.java
+++ chapter_004/src/main/java/ru/job4j/Calculator.java
@@ -15,43 +15,6 @@ import java.util.function.Function;
 public class Calculator {
 
     /**
-     * calcLin
-     * Calculates linear expression for a range of values.
-     *
-     * @param start - start value.
-     * @param end   - end value.
-     * @return - result list.
-     */
-    public List<Double> calcLin(int start, int end) {
-        return this.diapason(start, end, value -> 2 * value + 1);
-    }
-
-    /**
-     * calcSquare
-     * Calculates square expression for a range of values.
-     *
-     * @param start - start value.
-     * @param end   - end value.
-     * @return - result list.
-     */
-    public List<Double> calcSquare(int start, int end) {
-        return this.diapason(start, end, value -> Math.pow(value, 2) + 1);
-    }
-
-    /**
-     * calcLog
-     * Calculates logarithmic expression for a range of values.
-     *
-     * @param start - start value.
-     * @param end   - end value.
-     * @return - result list.
-     */
-    public List<Double> calcLog(int start, int end) {
-        return this.diapason(start, end, value -> new BigDecimal(Math.log(value) + 1)
-                                                        .setScale(2, RoundingMode.HALF_UP).doubleValue());
-    }
-
-    /**
      * diapason
      * Calculates the gives expression.
      *
@@ -60,7 +23,7 @@ public class Calculator {
      * @param func  - an instance of Functional interface.
      * @return - result list.
      */
-    private List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> res = new ArrayList<>();
         for (int i = start; i < end; i++) {
             res.add(func.apply(new Double(i)));
--- chapter_004/src/test/java/ru/job4j/CalculatorTest.java
+++ chapter_004/src/test/java/ru/job4j/CalculatorTest.java
@@ -3,6 +3,8 @@ package ru.job4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,31 +27,33 @@ public class CalculatorTest {
     }
 
     /**
-     * tests calcLin
+     * tests diapason
      */
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        List<Double> actual = this.calculator.calcLin(5, 8);
+        List<Double> actual = this.calculator.diapason(5, 8, value -> 2 * value + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(actual, is(expected));
     }
 
     /**
-     * tests calcSquare
+     * tests diapason
      */
     @Test
     public void whenSquareFunctionThenSquareResults() {
-        List<Double> actual = this.calculator.calcSquare(5, 8);
+        List<Double> actual = this.calculator.diapason(5, 8, value -> Math.pow(value, 2) + 1);
         List<Double> expected = Arrays.asList(26D, 37D, 50D);
         assertThat(actual, is(expected));
     }
 
     /**
-     * tests calcLog
+     * tests diapason
      */
     @Test
     public void whenLogFunctionThenLogResults() {
-        List<Double> actual = this.calculator.calcLog(5, 8);
+        List<Double> actual = this.calculator.diapason(5, 8,
+                                                        value -> new BigDecimal(Math.log(value) + 1)
+                                                                .setScale(2, RoundingMode.HALF_UP).doubleValue());
         List<Double> expected = Arrays.asList(2.61, 2.79, 2.95);
         assertThat(actual, is(expected));
     }
